e52eb21bd159e22c82888896b491d4c4dfce05df
22007234@mvula.univen.ac.za <126168942+22007234@users.noreply.github.com>
2024-03-15 19:09:59 +0200

    Add files via upload

---
+++ Linda.11.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double merchandiseCost, employeesSalary, yearlyRent, electricityCost;
+    double desiredProfitPercentage = 0.10;
+    double saleDiscountPercentage = 0.15;
+
+
+    cout << "Enter the total cost of merchandise: ";
+    cin >> merchandiseCost;
+    cout << "Enter the salary of employees (including Linda's own salary): ";
+    cin >> employeesSalary;
+    cout << "Enter the yearly rent: ";
+    cin >> yearlyRent;
+    cout << "Enter the estimated electricity cost: ";
+    cin >> electricityCost;
+
+    double totalExpenses = merchandiseCost + employeesSalary + yearlyRent + electricityCost;
+
+
+    double targetRevenue = totalExpenses / (1 - desiredProfitPercentage);
+
+
+    double sellingPriceAfterDiscount = targetRevenue / (1 - saleDiscountPercentage);
+
+
+    double markup = sellingPriceAfterDiscount - merchandiseCost;
+
+    cout << "The merchandise should be marked up by approximately $" << markup << endl;
+
+    return 0;
+}
